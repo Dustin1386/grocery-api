146c623c6dc4fce210ddd028a62fe8261f64f4f5
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2021-01-14 12:25:29 -0700

    ssl

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,7 +5,7 @@ module.exports = {
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
      ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL +"?SSL=true",
+    : process.env.DATABASE_URL +"?ssl=true",
 }
 // "host": process.env.PROD_MIGRATION_DB_HOST,
 //   "port": process.env.PROD_MIGRATION_DB_PORT,
