e8ea1d11770857dc6c61651e91c954997b8238fc
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2021-01-14 12:20:22 -0700

    package json

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "connectionString": (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL,
+  "connectionString": (process.env.NODE_ENV === 'test')
+     ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
 // "host": process.env.PROD_MIGRATION_DB_HOST,
 //   "port": process.env.PROD_MIGRATION_DB_PORT,
